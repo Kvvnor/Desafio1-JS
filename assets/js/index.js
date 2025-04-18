9b8ec262ff0dd66eddb93bff60a3a71150839699
Kevin Ortiz <kvvinortiz@gmail.com>
2025-04-17 21:30:36 -0400

    Establezco valor total en /usr/bin/bash

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,6 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+var totalSpan = document.querySelector(".valor-total"); 
+totalSpan.innerHTML = 0
